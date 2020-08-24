40d63325671fb809ddef4cc5cca9e8a267cb6c51
Jeremy Wood <js.wood206@gmail.com>
2020-08-24 20:08:25 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,10 @@
 def oxford_comma(array)
-
-new_string = array.join
-
+  if array.length < 2
+    new_string = array.join
+  elsif array.length == 2
+    new_string = array.join(" and ")
+  elsif array.length > 2
+    array[-1] = "and " + array[-1] 
+    new_string = array.join(", ")
+  end
 end
